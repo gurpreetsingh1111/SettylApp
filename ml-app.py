ea0d9e07402892e12ed4f88ca6d9f6dca7b251a6
Gurpreet2512Singh <84591513+gurpreetsingh1111@users.noreply.github.com>
2024-04-07 00:27:31 +0530

    Update ml-app.py

--- ml-app.py
+++ ml-app.py
@@ -10,6 +10,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 import matplotlib.pyplot as plt
 import nltk
+
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
